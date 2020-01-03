a4d66cd561647679d416008bfa31c7cd94f89d06
kamhar ngado <52423991+kamhar-ngado@users.noreply.github.com>
2020-01-03 17:30:41 +0700

    add eventlet

--- drive.py
+++ drive.py
@@ -1,8 +1,12 @@
+import socketio
+import eventlet
 from flask import Flask
+
+sio = socketio.Server()
 
 app = Flask(__name__)
 
-
-
 if __name__ == '__main__':
     app.run(port=3000)
+    app = socketio.Midleware(sio, app)
+    eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
