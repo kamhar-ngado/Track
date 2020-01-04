47755215b1a4c11cd04ea68fb6dbb4798a5cce1f
kamhar ngado <52423991+kamhar-ngado@users.noreply.github.com>
2020-01-05 06:28:02 +0700

    test drive 1

--- drive.py
+++ drive.py
@@ -7,45 +7,46 @@ import base64
 from io import BytesIO
 from PIL import Image
 import cv2
-
-
+ 
 sio = socketio.Server()
-
-app = Flask(__name__)
-
-# this is from google colab
+ 
+app = Flask(__name__) #'__main__'
+speed_limit = 10
 def img_preprocess(img):
-    img = img[60:135, :, :]
+    img = img[60:135,:,:]
     img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
-    img = cv2.GaussianBlur(img, (3, 3), 0)
+    img = cv2.GaussianBlur(img,  (3, 3), 0)
     img = cv2.resize(img, (200, 66))
     img = img/255
     return img
-
-
+ 
+ 
 @sio.on('telemetry')
 def telemetry(sid, data):
-    image = Image.open(bytesIO(base64.b64decode(data['image'])))
+    speed = float(data['speed'])
+    image = Image.open(BytesIO(base64.b64decode(data['image'])))
     image = np.asarray(image)
     image = img_preprocess(image)
     image = np.array([image])
-    streering_angle = float(model.predict(image))
-    send_control(steering_angle, 1.0)
-
-
+    steering_angle = float(model.predict(image))
+    throttle = 1.0 - speed/speed_limit
+    print('{} {} {}'.format(steering_angle, throttle, speed))
+    send_control(steering_angle, throttle)
+ 
+ 
+ 
 @sio.on('connect')
 def connect(sid, environ):
     print('Connected')
     send_control(0, 0)
-
-
+ 
 def send_control(steering_angle, throttle):
-    sio.emit('steer', data={
+    sio.emit('steer', data = {
         'steering_angle': steering_angle.__str__(),
         'throttle': throttle.__str__()
     })
-
-
+ 
+ 
 if __name__ == '__main__':
     model = load_model('model.h5')
     app = socketio.Middleware(sio, app)
