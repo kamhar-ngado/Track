4b9c489f31b4842e37fc0854d3a2dba7108b4c94
kamhar ngado <52423991+kamhar-ngado@users.noreply.github.com>
2020-01-05 05:11:29 +0700

    add opencv

--- drive.py
+++ drive.py
@@ -1,15 +1,26 @@
 import socketio
 import eventlet
+import numpy as np
 from flask import Flask
 from keras.models import load_model
 import base64
 from io import BytesIO
 from PIL import Image
+import cv2
 
 
 sio = socketio.Server()
 
 app = Flask(__name__)
+
+#this is from google colab
+def img_preprocess(img):
+  img = img[60:135,:,:]
+  img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+  img = cv2.GaussianBlur(img, (3,3), 0)
+  img = cv2.resize(img, (200, 66))
+  img = img/255
+  return img
 
 @sio.on('telemetry')
 def telemetry(sid, data):
